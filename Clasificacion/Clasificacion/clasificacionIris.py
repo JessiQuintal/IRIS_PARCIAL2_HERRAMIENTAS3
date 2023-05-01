30ceaa310e92376fa3e386ae85c503e51edbf1dc
Jessiquintalg <101346341+Jessiquintalg@users.noreply.github.com>
2023-05-01 18:52:54 -0500

    updates

--- Clasificacion/Clasificacion/clasificacionIris.py
+++ Clasificacion/Clasificacion/clasificacionIris.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import streamlit as st
+import os
+import importlib
 
 hide_streamlit_style= """
 <style>
@@ -9,17 +11,17 @@ footer {visibility:hidden;}
 """
 
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-URL= 'https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv'
-dfIris= pd.read_csv(URL)
 
-print(dfIris.head())
 st.title("Análisis estadistico de Iris Dataset")
-st.header("Estadisticas")
-st.write("Filas, Columnas:")
-st.write(dfIris.shape)
+st.header("En las siguientes páginas se presentan estadísticas, gráficas y modelo.")
 
-st.write("Describe:")
-st.dataframe(dfIris.describe())
 
-st.write("Clases:")
-st.write(dfIris["variety"].value_counts())
+# Obtener todas las páginas .py en la carpeta Pages
+pages = [f[:-3] for f in os.listdir("Clasificacion/Pages") if f.endswith(".py")]
+
+# Agregar la página seleccionada a la barra lateral
+selection = st.sidebar.radio("Pages:", pages)
+
+# Importar la página seleccionada y ejecutarla
+page_module = importlib.import_module(f"Pages.{selection}")
+page_module.show()
